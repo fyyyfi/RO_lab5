477ccd1da48decdeea0f7f80fb0cef0ebdb3c38e
Kateryna <katyuha.mik@gmail.com>
2025-11-16 14:39:05 +0000

    Serial alghorytm

---
+++ Serial.h
@@ -0,0 +1,7 @@
+int Min(int A, int B);
+void ProcessInitialization(int *&pMatrix, int& Size);
+void ProcessInitializationTest(int *&pMatrix, int& Size);
+void ProcessTermination(int *pMatrix);
+void DummyDataInitialization(int *pMatrix, int Size);
+void RandomDataInitialization(int *pMatrix, int Size);
+void SerialFloyd(int *pMatrix, int Size);
